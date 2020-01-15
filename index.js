4f0bce6cd883378343521ad2783064c312c5aeaa
edenbenbow <edenbenbow@users.noreply.github.com>
2020-01-14 16:03:43 -0800

    Removed useUnifiedTopology

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require('./passport');
 
 //mongoose.set('useFindAndModify', false);
 //mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://edenbenbow:testpw12345@watchr-fg1rx.mongodb.net/Watchr?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://edenbenbow:testpw12345@watchr-fg1rx.mongodb.net/Watchr?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 //Middleware functions
 
