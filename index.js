4956e868afe6c68308cd067959e9d297591bc14d
edenbenbow <edenbenbow@users.noreply.github.com>
2020-01-14 16:42:25 -0800

    Added UnifiedTopology back

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ require('./passport');
 
 //mongoose.set('useFindAndModify', false);
 //mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://edenbenbow:testpw12345@watchr-fg1rx.mongodb.net/Watchr?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect(
+    'mongodb+srv://edenbenbow:testpw12345@watchr-fg1rx.mongodb.net/Watchr?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useFindandModify: false,
+      useUnifiedTopology: true
+    }
+  );
+  
+  .catch(err => console.log(err.message));
 
 //Middleware functions
 
