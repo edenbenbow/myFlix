24a406b0157f044fd6e83b8dbd9a7d15925def18
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-01-26 11:44:21 -0800

    Adding scss

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Button, CardDeck, Container } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onClick } = this.props;
 
     return (
-      <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div>
+
+            <Card style={{ minWidth: '240px', maxWidth:'240px' }}>
+              <Card.Img variant="top" src={movie.ImagePath} />
+              <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>{movie.Description}</Card.Text>
+                <Button onClick={() => onClick(movie)} variant="link">Learn more</Button>
+              </Card.Body>
+            </Card>
+
     );
   }
 }
+
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string, // isRequired
+    imagePath: PropTypes.string, //isrequired
+    genre: PropTypes.string //isRequired
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
