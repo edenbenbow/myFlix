9e885c0de78d248f1fea5f6f46f1666a70b0d95e
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-06 06:46:34 -0800

    Adding new movie view

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import { Card, Container, ListGroup } from 'react-bootstrap';
+import {Button, Card, Container, Form, ListGroup} from 'react-bootstrap';
 import './movie-view.scss';
+
+import { Link } from 'react-router-dom';
+import axios from "axios";
 
 export class MovieView extends React.Component {
 
   constructor() {
     super();
-
     this.state = {};
+
+    this.state = {
+      isFavoriteSelected: false,
+    };
+  }
+
+  addFavorite(event) {
+    const { user, movie } = this.props;
+    event.preventDefault();
+    axios.post(`https://watchrdb.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+        {Username: localStorage.getItem('user')},
+        {headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }}
+    )
+    .then(response => {
+          alert(`${movie.Title} has been successfully added!`);
+          document.location.reload(true);
+    })
+    .catch(error => {
+          alert(`We were unable to add ${movie.Title} to your favorites: ` + error);
+    });
+  };
+
+  removeFavorite(event) {
+    const { user, movie } = this.props;
+    event.preventDefault();
+    axios.delete(`https://watchrdb.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+        .then(response => {
+          alert(`${movie.Title} has been removed from favorites`);
+          document.location.reload(true);
+        })
+
+        .catch(error => {
+          alert('Unable to remove movie from favorites: ' + error)
+        });
   }
 
   goBack() {
@@ -15,45 +53,67 @@ export class MovieView extends React.Component {
   }
 
   render() {
-    const { movie } = this.props;
+    //console.log("this.props: " + JSON.stringify(this.props));
+    const { movie, user } = this.props;
 
-    if (!movie) return null;
+    const favoriteEdit = (e) => {
+      this.setState({
+        isFavoriteSelected: true
+      })
+    }
+
+
+    if (!movie || !user) return null;
 
     return (
-        <Card style={{ width: '10 rem'}}>
-          <div className="movie-view">
-            <img className="movie-poster" src={movie.ImagePath} />
-            <ListGroup varient="flush">
-              <ListGroup.Item>
-                <div className="movie-title">
+      <Card style={{ width: '10 rem' }}>
+        <div className="movie-view">
+          <img className="movie-poster" src={movie.ImagePath} />
+          <ListGroup varient="flush">
+            <ListGroup.Item>
+              <div className="movie-title">
                 <span className="label">Title: </span>
                 <span className="value">{movie.Title}</span>
+                {
+                  //this.state.isFavoriteSelected ?
+                  user.FavoriteMovies.includes(movie._id) ?
+                    <Button onClick={this.removeFavorite.bind(this)} variant="outline-warning">Unfavorite</Button> :
+                    <Button onClick={this.addFavorite.bind(this)} variant="warning">Favorite</Button>
+                }
               </div>
-              </ListGroup.Item>
-              <ListGroup.Item>
+            </ListGroup.Item>
+            <ListGroup.Item>
               <div className="movie-description">
                 <span className="label">Description: </span>
                 <span className="value">{movie.Description}</span>
               </div>
-              </ListGroup.Item>
-              <ListGroup.Item>
+            </ListGroup.Item>
+            <ListGroup.Item>
               <div className="movie-genre">
                 <span className="label">Genre: </span>
-                <span className="value">{movie.Genre.Name}</span>
+                <span className="value">
+                  <Link to={`/genres/${movie.Genre.Name}`}>
+                    {movie.Genre.Name}
+                  </Link>
+                </span>
               </div>
-              </ListGroup.Item>
-              <ListGroup.Item>
+            </ListGroup.Item>
+            <ListGroup.Item>
               <div className="movie-director">
                 <span className="label">Director: </span>
-                <span className="value">{movie.Director.Name}</span>
+                <span className="value">
+                  <Link to={`/directors/${movie.Director.Name}`}>
+                    {movie.Director.Name}
+                  </Link>
+                </span>
               </div>
-              </ListGroup.Item>
-              <ListGroup.Item>
-              <button className="back-to-main" onClick={this.goBack}>Back</button>
-              </ListGroup.Item>
-            </ListGroup>
-          </div >
-        </Card>
-    );
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <button className="back-to-main" onClick={this.goBack}>Browse all movies</button>
+            </ListGroup.Item>
+          </ListGroup>
+        </div >
+      </Card>
+    )
+    }
   }
-}
