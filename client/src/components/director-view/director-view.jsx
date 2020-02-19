281dfe873bffebc6cc2ef6f1b4d09a6229df3896
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-19 13:22:39 -0800

    Adding PropTypes

--- client/src/components/director-view/director-view.jsx
+++ client/src/components/director-view/director-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Card, Button, CardDeck, ListGroup} from 'react-bootstrap';
 
 import './director-view.scss';
@@ -8,9 +9,12 @@ import MoviesList from '../movies-list/movies-list';
 
 
 function DirectorView(props) {
-    const {movies, director} = props;
+    const {movies, directorName} = props;
 
     if (!movies || !movies.length) return null;
+
+    let filteredMovies = movies.filter((m) => m.Director.Name === directorName);
+
 
     return (
       <Card style={{ width: '10 rem'}}>
@@ -18,26 +22,26 @@ function DirectorView(props) {
                     <ListGroup varient="flush">
                         <ListGroup.Item>
                             <div className="director-name">
-                                <h2 className="value">{director.Name}</h2>
+                                <h2 className="value">{filteredMovies[0].Director.Name}</h2>
                             </div>
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <div className="director-birth">
                                 <span className="label">Birth date: </span>
-                                <span className="value">{director.Birth}</span>
+                                <span className="value">{filteredMovies[0].Director.Birth}</span>
                             </div>
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <div className="director-birth">
                                 <span className="label">Bio: </span>
-                                <span className="value">{director.Bio}</span>
+                                <span className="value">{filteredMovies[0].Director.Bio}</span>
                             </div>
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <div className="movies-list">
                                 <span className="label">Movies: </span>
                                 <span className="value">
-                                  <MoviesList movies={movies}/>
+                                  <MoviesList movies={filteredMovies}/>
                                 </span>
                             </div>
                         </ListGroup.Item>
@@ -54,3 +58,11 @@ let mapStateToProps = state => {
 };
 
 export default connect(({ movies }) => ({ movies }))(DirectorView);
+
+DirectorView.propTypes = {
+    Director: PropTypes.shape({
+        Name: PropTypes.string,
+        Bio: PropTypes.string,
+        Birth: PropTypes.string
+    }).isRequired
+};
