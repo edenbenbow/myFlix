399edc78c0ace021b49400cafdb6824feccdc38f
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-19 13:21:18 -0800

    Removing sort

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,7 +1,8 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
-export const SET_SORT = 'SET_SORT';
 export const SET_USER = 'SET_USER';
+export const SET_USERS = 'SET_USERS';
+//export const SET_FAVORITE = 'SET_FAVORITE';
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -14,6 +15,14 @@ export function setSort(value) {
 export function setUser(value) {
   return { type: SET_USER, value}
 }
+
+export function setUsers(value) {
+  return { type: SET_USERS, value}
+}
+
+//export function setFavorite(value) {
+  //return { type: SET_FAVORITE, value };
+//}
 
 export function setFilter(value) {
   return { type: SET_FILTER, value };
