890daa31a9e4fe821e18d7561fc2ad4a65d918e8
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-20 08:24:44 -0800

    Styling for mobile

--- client/src/components/movies-list/movies-list.jsx
+++ client/src/components/movies-list/movies-list.jsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { CardDeck, Container } from 'react-bootstrap';
-import './movies-list.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { CardDeck, Container } from "react-bootstrap";
+import "./movies-list.scss";
 
-import { MovieCard } from '../movie-card/movie-card';
-import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
+import { MovieCard } from "../movie-card/movie-card";
+import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
 
 const mapStateToProps = state => {
-  const { visibilityFilter } = state;
-  return { visibilityFilter };
+    const { visibilityFilter } = state;
+    return { visibilityFilter };
 };
 
 function MoviesList(props) {
-  const { movies, visibilityFilter } = props;
-  let filteredMovies = movies;
+    const { movies, visibilityFilter } = props;
+    let filteredMovies = movies;
 
-  if (visibilityFilter !== '') {
-    filteredMovies = movies.filter(m => m.Title.includes(visibilityFilter));
-  }
+    if (visibilityFilter !== "") {
+        filteredMovies = movies.filter(m => m.Title.includes(visibilityFilter));
+    }
 
-  if (!movies) return <div className="main-view"/>;
+    if (!movies) return <div className="main-view" />;
 
-  return (
-      <Container>
-        <VisibilityFilterInput id="filterField"/>
-        <CardDeck>
-      {filteredMovies.map(m => <MovieCard key={m._id} movie={m}/>)}
-        </CardDeck>
-      </Container>)
+    return (
+        <Container>
+            <VisibilityFilterInput id="filterField" />
+            <CardDeck className="movie-card">
+                {filteredMovies.map(m => (
+                    <MovieCard key={m._id} movie={m} />
+                ))}
+            </CardDeck>
+        </Container>
+    );
 }
 
 export default connect(mapStateToProps)(MoviesList);
