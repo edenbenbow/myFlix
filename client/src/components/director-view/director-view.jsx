ab5cb9445d102ad4a34e89e6bda20882655ea7a2
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-20 08:24:09 -0800

    Styling for mobile

--- client/src/components/director-view/director-view.jsx
+++ client/src/components/director-view/director-view.jsx
@@ -1,60 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Card, Button, CardDeck, ListGroup} from 'react-bootstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import { Card, Button, CardDeck, ListGroup } from "react-bootstrap";
 
-import './director-view.scss';
+import "./director-view.scss";
 
-import { connect } from 'react-redux';
-import MoviesList from '../movies-list/movies-list';
-
+import { connect } from "react-redux";
+import MoviesList from "../movies-list/movies-list";
 
 function DirectorView(props) {
-    const {movies, directorName} = props;
+    const { movies, directorName } = props;
 
     if (!movies || !movies.length) return null;
 
-    let filteredMovies = movies.filter((m) => m.Director.Name === directorName);
-
+    let filteredMovies = movies.filter(m => m.Director.Name === directorName);
 
     return (
-      <Card style={{ width: '10 rem'}}>
-                <div className="director-view">
-                    <ListGroup varient="flush">
-                        <ListGroup.Item>
-                            <div className="director-name">
-                                <h2 className="value">{filteredMovies[0].Director.Name}</h2>
-                            </div>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <div className="director-birth">
-                                <span className="label">Birth date: </span>
-                                <span className="value">{filteredMovies[0].Director.Birth}</span>
-                            </div>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <div className="director-birth">
-                                <span className="label">Bio: </span>
-                                <span className="value">{filteredMovies[0].Director.Bio}</span>
-                            </div>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <div className="movies-list">
-                                <span className="label">Movies: </span>
-                                <span className="value">
-                                  <MoviesList movies={filteredMovies}/>
-                                </span>
-                            </div>
-                        </ListGroup.Item>
-
-                  </ListGroup>
-                </div>
-            </Card>
-
-        );
-    }
+        <Card style={{ width: "10 rem" }}>
+            <div className="director-view">
+                <ListGroup varient="flush">
+                    <ListGroup.Item>
+                        <div className="director-name">
+                            <h2 className="value">{filteredMovies[0].Director.Name}</h2>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <div className="director-birth">
+                            <span className="label">Birth date: </span>
+                            <span className="value">{filteredMovies[0].Director.Birth}</span>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <div className="director-birth">
+                            <span className="label">Bio: </span>
+                            <span className="value">{filteredMovies[0].Director.Bio}</span>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <div className="movies-list">
+                            <span className="label">Movies: </span>
+                            <span className="value">
+                <MoviesList movies={filteredMovies} />
+              </span>
+                        </div>
+                    </ListGroup.Item>
+                </ListGroup>
+            </div>
+        </Card>
+    );
+}
 
 let mapStateToProps = state => {
-    return { movies: state.movies }
+    return { movies: state.movies };
 };
 
 export default connect(({ movies }) => ({ movies }))(DirectorView);
