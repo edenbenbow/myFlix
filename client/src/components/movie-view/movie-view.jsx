45a3b79ecb1812e2d7a734e2ba245020f8d91cf1
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-20 08:25:13 -0800

    Prettier

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,73 +1,85 @@
-import React from 'react';
-import {Button, Card, Container, Form, ListGroup} from 'react-bootstrap';
-import './movie-view.scss';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { Link } from 'react-router-dom';
+import { Button, Card, Container, Form, ListGroup } from "react-bootstrap";
+import "./movie-view.scss";
+
+import { Link } from "react-router-dom";
 import axios from "axios";
 
-import { connect } from 'react-redux';
-import {setMovies, setUser, setUsers} from "../../actions/actions";
-import {MainView} from "../main-view/main-view";
-
+import { connect } from "react-redux";
 
 function MovieView(props) {
-  const {user, movie, movies} = props;
+  const { user, movie, movies } = props;
 
   if (!user || !movies || !movies.length) return null;
 
   function addFavorite(event) {
     event.preventDefault();
-    axios.post(`https://watchrdb.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
-        {Username: localStorage.getItem('user')},
-        {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}}
-    )
+    axios
+        .post(
+            `https://watchrdb.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+            { Username: localStorage.getItem("user") },
+            {
+              headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+            }
+        )
         .then(response => {
           alert(`${movie.Title} has been successfully added!`);
           document.location.reload(true);
         })
         .catch(error => {
-          alert(`We were unable to add ${movie.Title} to your favorites: ` + error);
-        })
-
+          alert(
+              `We were unable to add ${movie.Title} to your favorites: ` + error
+          );
+        });
   }
 
   function removeFavorite(event) {
     event.preventDefault();
-    axios.delete(`https://watchrdb.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
-      headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-    })
+    axios
+        .delete(
+            `https://watchrdb.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+            {
+              headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+            }
+        )
         .then(response => {
           alert(`${movie.Title} has been removed from favorites`);
           document.location.reload(true);
         })
 
         .catch(error => {
-          alert('Unable to remove movie from favorites: ' + error)
+          alert("Unable to remove movie from favorites: " + error);
         });
   }
 
   function goBack() {
-    console.log("Test4");
-
-    window.location = '/';
+    window.location = "/";
   }
 
   return (
-
-      <Card style={{width: '10 rem'}}>
+      <Card style={{ width: "10 rem" }}>
         <div className="movie-view">
-          <img className="movie-poster" src={movie.ImagePath}/>
+          <img className="movie-poster" src={movie.ImagePath} />
           <ListGroup varient="flush">
             <ListGroup.Item>
               <div className="movie-title">
                 <span className="label">Title: </span>
                 <span className="value">{movie.Title}</span>
-                {
-                  user.FavoriteMovies.includes(movie._id) ?
-                      <Button onClick={removeFavorite} variant="outline-warning">Unfavorite</Button> :
-                      <Button onClick={addFavorite} variant="warning">Favorite</Button>
-                }
-
+                {user.FavoriteMovies.includes(movie._id) ? (
+                    <Button
+                        onClick={removeFavorite}
+                        variant="outline-secondary"
+                        id="remove"
+                    >
+                      Unfavorite
+                    </Button>
+                ) : (
+                    <Button onClick={addFavorite} variant="warning">
+                      Favorite
+                    </Button>
+                )}
               </div>
             </ListGroup.Item>
             <ListGroup.Item>
@@ -80,24 +92,26 @@ function MovieView(props) {
               <div className="movie-genre">
                 <span className="label">Genre: </span>
                 <span className="value">
-                  <Link to={`/genres/${movie.Genre.Name}`}>
-                    {movie.Genre.Name}
-                  </Link>
-                </span>
+                <Link to={`/genres/${movie.Genre.Name}`}>
+                  {movie.Genre.Name}
+                </Link>
+              </span>
               </div>
             </ListGroup.Item>
             <ListGroup.Item>
               <div className="movie-director">
                 <span className="label">Director: </span>
                 <span className="value">
-                  <Link to={`/directors/${movie.Director.Name}`}>
-                    {movie.Director.Name}
-                  </Link>
-                </span>
+                <Link to={`/directors/${movie.Director.Name}`}>
+                  {movie.Director.Name}
+                </Link>
+              </span>
               </div>
             </ListGroup.Item>
             <ListGroup.Item>
-              <button className="back-to-main" onClick={goBack}>Browse all movies</button>
+              <button className="back-to-main" onClick={goBack}>
+                Browse all movies
+              </button>
             </ListGroup.Item>
           </ListGroup>
         </div>
@@ -105,5 +119,23 @@ function MovieView(props) {
   );
 }
 
-
 export default connect(({ movies }) => ({ movies }))(MovieView);
+
+MovieView.propTypes = {
+  user: PropTypes.shape({
+    Username: PropTypes.string
+  }),
+  movie: PropTypes.shape({
+    _id: PropTypes.string,
+    Title: PropTypes.string,
+    ImagePath: PropTypes.string,
+    Description: PropTypes.string,
+    Genre: PropTypes.exact({
+      Name: PropTypes.string,
+      Description: PropTypes.string
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string
+    })
+  }).isRequired
+};
