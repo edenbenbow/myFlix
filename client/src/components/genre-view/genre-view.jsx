df75c7619d112da3e4fcccef5be3020e736cd424
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-20 08:23:35 -0800

    Styling for mobile

--- client/src/components/genre-view/genre-view.jsx
+++ client/src/components/genre-view/genre-view.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import {Card, ListGroup, Button, CardDeck} from 'react-bootstrap';
+import React from "react";
+import { Card, ListGroup, Button, CardDeck } from "react-bootstrap";
 
-import './genre-view.scss';
+import "./genre-view.scss";
 
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
-import MoviesList from '../movies-list/movies-list';
+import { connect } from "react-redux";
+import MoviesList from "../movies-list/movies-list";
 import PropTypes from "prop-types";
-
-
 
 function GenreView(props) {
     const { movies, genreName } = props;
@@ -16,45 +14,44 @@ function GenreView(props) {
 
     if (!movies || !movies.length || !genreName) return null;
 
-    let filteredMovies = movies.filter((m) => m.Genre.Name === genreName);
+    let filteredMovies = movies.filter(m => m.Genre.Name === genreName);
 
     return (
-      <Card style={{ width: '20 rem'}}>
-        <div className="genre-view">
-          <ListGroup varient="flush">
-            <ListGroup.Item>
-              <div className="genre-name">
-                <h2 className="value">{filteredMovies[0].Genre.Name}</h2>
-              </div>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <div className="genre-description">
-                <span className="label">Description: </span>
-                <span className="value">{filteredMovies[0].Genre.Description}</span>
-              </div>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <div className="genre-description">
-                <span className="label">Movies: </span>
-                <span className="value">
-                  <CardDeck>
-                    <MoviesList movies={filteredMovies}/>
-                  </CardDeck>
-                </span>
-              </div>
-            </ListGroup.Item>
-          </ListGroup>
-        </div>
-      </Card>
-
+        <Card style={{ width: "20 rem" }}>
+            <div className="genre-view">
+                <ListGroup varient="flush">
+                    <ListGroup.Item>
+                        <div className="genre-name">
+                            <h2 className="value">{filteredMovies[0].Genre.Name}</h2>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <div className="genre-description">
+                            <span className="label">Description: </span>
+                            <span className="value">
+                {filteredMovies[0].Genre.Description}
+              </span>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <div className="genre-description">
+                            <span className="label">Movies: </span>
+                            <span className="value">
+                  <MoviesList movies={filteredMovies} />
+              </span>
+                        </div>
+                    </ListGroup.Item>
+                </ListGroup>
+            </div>
+        </Card>
     );
 }
 
-export default connect(({movies}) => ({movies}))(GenreView);
+export default connect(({ movies }) => ({ movies }))(GenreView);
 
 GenreView.propTypes = {
     Genre: PropTypes.shape({
         Name: PropTypes.string,
-        Description: PropTypes.string,
+        Description: PropTypes.string
     }).isRequired
 };
