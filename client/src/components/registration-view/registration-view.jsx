a0784f63036c647dd8349166409c2776c4220b52
Eden Benbow <37776140+edenbenbow@users.noreply.github.com>
2020-02-20 08:30:38 -0800

    Prettier

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
-import './registration-view.scss';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import "./registration-view.scss";
+import axios from "axios";
 
 import { Link } from "react-router-dom";
 
 export function RegistrationView(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [birthday, setBirthday] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthday, setBirthday] = useState("");
 
-
-    const handleSubmit = (e) => {
+    const handleSubmit = e => {
         e.preventDefault();
         console.log(username, password, birthday, email);
         /* Send a request to the server for authentication */
-        axios.post('https://watchrdb.herokuapp.com/users', {
-            username: username,
-            password: password,
-            birthday: birthday,
-            email: email,
-        })
-        .then(response => {
-            const data = response.data;
-            console.log(data);
-            window.open('/', '_self');
-        })
-        .catch(e => {
-            console.log('Error registering user.');
-        });
+        axios
+            .post("https://watchrdb.herokuapp.com/users", {
+                username: username,
+                password: password,
+                birthday: birthday,
+                email: email
+            })
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                window.open("/", "_self");
+            })
+            .catch(e => {
+                console.log("Error registering user.");
+            });
     };
 
     return (
@@ -41,7 +41,7 @@ export function RegistrationView(props) {
                         type="text"
                         placeholder="Enter username"
                         value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                        onChange={e => setUsername(e.target.value)}
                     />
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
@@ -49,7 +49,7 @@ export function RegistrationView(props) {
                         type="text"
                         placeholder="Enter Password"
                         value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </Form.Group>
                 <Form.Group controlId="formBasicEmail">
@@ -57,7 +57,7 @@ export function RegistrationView(props) {
                         type="text"
                         placeholder="Enter Email"
                         value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </Form.Group>
                 <Form.Group controlId="formBasicBirthday">
@@ -65,15 +65,14 @@ export function RegistrationView(props) {
                         type="text"
                         placeholder="Enter Date of Birth"
                         value={birthday}
-                        onChange={(e) => setBirthday(e.target.value)}
+                        onChange={e => setBirthday(e.target.value)}
                     />
                 </Form.Group>
                 <Button variant="primary" type="submit" onClick={handleSubmit}>
                     Sign up
                 </Button>
-                <Link to={`/`}><Button variant="outline-secondary">
-                    Sign in
-                </Button>
+                <Link to={`/`}>
+                    <Button variant="outline-secondary">Sign in</Button>
                 </Link>
             </Form>
         </Container>
